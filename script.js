f2cee61f97fbfc8986bc98fba043edc82b324b45
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-02-24 11:51:54 +0530

    Updated footer

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let navItems = document.getElementsByClassName('nav-link')
+let date = new Date()
+currentYear = date.getFullYear(); 
 
-for(let i=0;i<navItems.length;i++){
-	navItems[i].addEventListener('mouseover',()=>{
-		navItems[i].style.color = 'rgb(0,255,0)'
-	})
-	navItems[i].addEventListener('mouseout',()=>{
-		navItems[i].style.color = 'rgb(255,255,255)'
-	})
-}
+let footer = document.getElementsByTagName('footer')
+footer[0].innerHTML += `<p class = 'copyrights'>Copyright &copy;  ${currentYear} <a class='github-link' href="https://github.com/Prathyusha-Guduru">Prathyusha Guduru</a>. All Rights Reserved</p>`
+
+
+// for(let i=0;i<navItems.length;i++){
+// 	navItems[i].addEventListener('mouseover',()=>{
+// 		navItems[i].style.color = 'rgb(0,255,0)'
+// 	})
+// 	navItems[i].addEventListener('mouseout',()=>{
+// 		navItems[i].style.color = 'rgb(255,255,255)'
+// 	})
+// }
 
 
