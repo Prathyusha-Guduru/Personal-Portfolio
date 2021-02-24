8d56c77f976ae07a6a2903191a751db60e8565c0
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-02-24 12:16:13 +0530

    Updated footer

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let date = new Date()
 currentYear = date.getFullYear(); 
 
 let footer = document.getElementsByTagName('footer')
-footer[0].innerHTML += `<p class = 'copyrights'>Copyright &copy;  ${currentYear} <a class='github-link' href="https://github.com/Prathyusha-Guduru">Prathyusha Guduru</a>. All Rights Reserved</p>`
+footer[0].innerHTML += `<p class = 'copyrights'>Copyright &copy;  ${currentYear} <a class='github-link' href="https://github.com/Prathyusha-Guduru">Prathyusha Guduru</a> | All Rights Reserved</p>`
 
 
 // for(let i=0;i<navItems.length;i++){
