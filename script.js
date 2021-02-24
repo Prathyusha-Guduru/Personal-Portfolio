60e84b564c8c7c8209c6915adfc91a51f0d26570
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-02-24 15:33:08 +0530

    Updated project-cards

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let footer = document.getElementsByTagName('footer')
 footer[0].innerHTML += `<p class = 'copyrights'>Copyright &copy;  ${currentYear} <a class='github-link' href="https://github.com/Prathyusha-Guduru">Prathyusha Guduru</a> | All Rights Reserved</p>`
 
 
-// for(let i=0;i<navItems.length;i++){
-// 	navItems[i].addEventListener('mouseover',()=>{
-// 		navItems[i].style.color = 'rgb(0,255,0)'
-// 	})
-// 	navItems[i].addEventListener('mouseout',()=>{
-// 		navItems[i].style.color = 'rgb(255,255,255)'
-// 	})
-// }
+for(let i=0;i<navItems.length;i++){
+	navItems[i].addEventListener('mouseover',()=>{
+		navItems[i].style.color = 'rgb(137, 243, 137)'
+	})
+	navItems[i].addEventListener('mouseout',()=>{
+		navItems[i].style.color = 'rgb(255,255,255)'
+	})
+}
 
 
